d79b2633f05796f43e78734b92e97a3f79281879
CH <ch_hsu@asus.com>
2017-09-28 16:17:15 +0800

    Auto-restart app with nodemon

--- app.js
+++ app.js
@@ -2,28 +2,11 @@ console.log('Starting app.js');
 
 // require the build-in module
 const fs = require('fs'); // file system
-const os = require('os'); // operating system-related
-
-// require the self-define file
-const notes = require('./notes.js'); // use the relative path
-//console.log('Result: ', notes.add(9, -2));
-
 // require 3rd party module
 const _ = require('lodash'); // the name in the package.jason file
 
-console.log(_.isString(true)); // false
-console.log(_.isString('CH')); // true
+// require the self-define file
+const notes = require('./notes.js'); // use the relative path
 
-var filteredArray = _.uniq(['CH', 1, 'CH', 1, 2, 3, 4]);
-console.log(filteredArray); // ['CH', 1, 2, 3, 4]
 
-/*
-var user = os.userInfo();
-console.log(user);
 
-fs.appendFile('greeting.txt', `Hello ${user.username}!`, (err) => {
-    if (err) {
-        console.log('fail to write the file');
-    }
-});
-*/
--- notes.js
+++ notes.js
@@ -1,12 +1,5 @@
 console.log('Starting notes.js');
 
-//console.log(module);
-
-/* 
-the exports project is a property of module and can be exported
-for other file.
-e.g. self-defined function reuse
-*/
 module.exports.add = (a, b) => {
     return a + b;
 };
