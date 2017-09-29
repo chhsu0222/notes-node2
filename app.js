3946a7c8d764bbd1b1087c8c7a37d5a9f8c54629
CH <ch_hsu@asus.com>
2017-09-29 11:12:59 +0800

    get user input with process.argv

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ const _ = require('lodash'); // the name in the package.jason file
 // require the self-define file
 const notes = require('./notes.js'); // use the relative path
 
+console.log(process.argv);
 
+var command = process.argv[2];
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Getting a note');
+} else if (command === 'remove') {
+    console.log('Removing a note');
+} else {
+    console.log('command not recognized');
+}
 
