2473d5de915d9bc749c48ef7c30ec53369f87506
CH <ch_hsu@asus.com>
2017-09-29 12:29:42 +0800

    Simplified input with Yargs

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ console.log('Starting app.js');
 const fs = require('fs'); // file system
 // require 3rd party module
 const _ = require('lodash'); // the name in the package.jason file
+// install yargs with the command: npm install yargs@4.7.1 --save  
+// @means version
+const yargs = require('yargs');
 
 // require the self-define file
 const notes = require('./notes.js'); // use the relative path
 
-console.log(process.argv);
 
-var command = process.argv[2];
+const argv = yargs.argv
+console.log('process:', process.argv);
+console.log('yargs:', argv);
+var command = argv._[0];
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.listNote();
 } else if (command === 'read') {
-    console.log('Getting a note');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing a note');
+    notes.removeNote(argv.title);
 } else {
     console.log('command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,5 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.add = (a, b) => {
-    return a + b;
+var addNote = (title, body) => {
+    console.log('Adding note:', title, body);
+};
+
+var listNote = () => {
+    console.log('Listing all notes');
+};
+
+var getNote = (title) => {
+    console.log('Reading note:', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note:', title);
+};
+
+module.exports = {
+    addNote, //  ES6 syntex since key is equal to the value,
+    listNote,  // same as listNode: listNode
+    getNote,
+    removeNote 
 };
