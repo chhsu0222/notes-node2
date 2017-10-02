306a3abf7ed053050f1a89ca242d282db1db5fa3
CH <ch_hsu@asus.com>
2017-10-02 09:53:35 +0800

    Adding and saving notes

--- notes.js
+++ notes.js
@@ -1,7 +1,42 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note:', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    /*
+    filter method returns a new array with the elements meet the criteria
+    It is like "forEach" method which takes a callback function
+    (element, index, array)
+    */
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    /*
+    same as 
+    var duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+    });
+    */
+
+    if (duplicateNotes.length === 0) {
+        // no duplicate notes
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
+
 };
 
 var listNote = () => {
